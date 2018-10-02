3fb9eaab242b4af8406e519d7e7517555ffefdc4
Jean-Nicolas Sénécal <jesenb@l3818-14.info.polymtl.ca>
2018-10-02 09:12:30 -0400

    tp6 depot

---
+++ TP6/Probleme1.cpp
@@ -0,0 +1,51 @@
+// /*
+//  * Nom Tp2P2.cpp
+//  * Auteur : Gagné S. et Sénécal J-N
+//  * Date : 14 septembre 2018
+//  * Machine a etat probleme 2 tp2
+//  */
+
+// /*
+//  * Pin de la LED connecte au PORTA(pin 1 et pin 2)
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+#define F_CPU 8000000
+#include <util/delay.h>
+#include "memoire_24.h"
+volatile uint8_t minuterieExpiree = 0; //-----p2
+volatile uint8_t boutonPoussoir = 0;   //-------p2
+
+
+int main()
+{
+	/*
+	class Memoire24 :: 
+	{
+	ecriture(0x00, "*P*O*L*Y*T*E*C*H*N*I*Q*U*E**M*O*N*T*R*E*A*L*");
+	_delay_ms(3000);
+	lecture(0x00, ecriture);
+	
+	if (ecriture ==lecture)
+	{
+	PORTA = 0b01;	
+	}
+	if(ecriture !=lecture)
+	{
+	PORTA = 0b10;	
+	}
+	lecture
+    DDRA = 0xff;
+    DDRB = 0xff; // PORT B est en mode sortie
+    DDRC = 0xff; // PORT C est en mode sortie
+    DDRD = 0x00; // PORT D est en mode entree
+    */
+    
+    uint8_t compteur = 0;
+}
+
+
+    
+    return 0;
+}
